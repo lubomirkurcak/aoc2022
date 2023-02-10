1c5ccf88066b1a8939ad80df49ef490d88f5f815
lubomirkurcak <lubomirkurcak@gmail.com>
2023-02-10 06:55:46 +0100

    promising progress !

--- src/day23.rs
+++ src/day23.rs
@@ -1,12 +1,17 @@
 #![allow(clippy::reversed_empty_ranges)]
 
-use ndarray::prelude::*;
-use std::{io::prelude::*, io::BufReader};
+use ndarray::{prelude::*, Zip};
+use std::io::BufReader;
 
-const INPUT: &[u8] = include_bytes!("../in23.txt");
-const N: usize = 74;
+// const INPUT: &[i8] = include_bytes!("../in23.txt");
+// const N: usize = 74;
+const INPUT: &[u8] = include_bytes!("../in23_small.txt");
+const N: usize = 7;
+// const PAD_SIZE: usize = N;
+const PAD_SIZE: usize = 2;
+const M: usize = N + 2 * PAD_SIZE;
 
-type Board = Array2<u8>;
+type Board = Array2<i8>;
 
 fn parse(x: &[u8]) -> Board {
     let a = Array::from_iter(x.iter().filter_map(|&b| match b {
@@ -16,57 +21,239 @@ fn parse(x: &[u8]) -> Board {
     }));
     let a = a.into_shape((N, N)).unwrap();
 
-    let pad_size = 2;
-    let mut map = Board::from_elem(((N + 2 * pad_size), (N + 2 * pad_size)), 0);
-    let pad_size: i32 = pad_size.try_into().unwrap();
+    let mut map = Board::from_elem((M, M), 0);
+    let pad_size: i32 = PAD_SIZE.try_into().unwrap();
     map.slice_mut(s![pad_size..-pad_size, pad_size..-pad_size])
         .assign(&a);
     map
 }
 
-fn iterate(z: &mut Board, scratch: &mut Board) {
-    // compute number of neighbors
-    let mut neigh = scratch.view_mut();
-    neigh.fill(0);
+const PX: i8 = 1; // East
+const PY: i8 = 2; // South
+const NX: i8 = 4; // West
+const NY: i8 = 8; // North
+const OCC: i8 = 16; // Occupied
 
-    // NOTE(lubo): This is really clever, I like it!
-    // But I'll have to actually have a (worst case) boundary pad of size N+1 on each side!
-    // Too tired to do this now but can't wait for it!
-    neigh += &z.slice(s![0..-2, 0..-2]);
-    neigh += &z.slice(s![0..-2, 1..-1]);
-    neigh += &z.slice(s![0..-2, 2..]);
+fn iterate(z: &mut Board, dir_order: &[i8]) {
+    println!("Current dir order {dir_order:?}");
 
-    neigh += &z.slice(s![1..-1, 0..-2]);
-    neigh += &z.slice(s![1..-1, 2..]);
+    let mut neigh_mask = Board::zeros((M - 2, M - 2));
+    let mut proposition_dirs = Board::zeros((M, M));
+    let mut proposition_ps = Board::zeros((M, M));
+    let mut move_tos = Board::zeros((M, M));
+    let mut move_froms = Board::zeros((M, M));
 
-    neigh += &z.slice(s![2.., 0..-2]);
-    neigh += &z.slice(s![2.., 1..-1]);
-    neigh += &z.slice(s![2.., 2..]);
+    neigh_mask.fill(0);
+    proposition_dirs.fill(0);
+    proposition_ps.fill(0);
+    move_tos.fill(0);
+    move_froms.fill(0);
 
-    // birth where n = 3 and z[i] = 0,
-    // survive where n = 2 || n = 3 and z[i] = 1
-    let mut zv = z.slice_mut(s![1..-1, 1..-1]);
+    // NOTE(lubo): Check if there are neighbours in each direction according to the problem definition
+    // We use 5 bits: <-- high [Occupied = 4][South = 3][West = 2][North = 1][East = 0] low -->
+    //   - Occupied if there is a guy at the tile
+    //   - <Direction> if there are neighbours in that direction
 
-    // this is autovectorized amazingly well!
-    zv.zip_mut_with(&neigh, |y, &n| *y = ((n == 3) || (n == 2 && *y > 0)) as u8);
+    let center = s![1..-1, 1..-1];
+    let nx = s![1..-1, ..-2];
+    let px = s![1..-1, 2..];
+    let ny = s![..-2, 1..-1];
+    let py = s![2.., 1..-1];
+
+    // (y,x)
+    // (-1,-1)
+    let slice = &z.slice(s![..-2, ..-2]);
+    neigh_mask = neigh_mask | (NX * slice);
+    neigh_mask = neigh_mask | (NY * slice);
+
+    // (-1,0)
+    let slice = &z.slice(ny);
+    neigh_mask = neigh_mask | (NY * slice);
+
+    // (-1,1)
+    let slice = &z.slice(s![..-2, 2..]);
+    neigh_mask = neigh_mask | (PX * slice);
+    neigh_mask = neigh_mask | (NY * slice);
+
+    // (0,-1)
+    let slice = &z.slice(nx);
+    neigh_mask = neigh_mask | (NX * slice);
+
+    // (0,0)
+    let slice = &z.slice(center);
+    neigh_mask = neigh_mask | (OCC * slice);
+
+    // (0,1)
+    let slice = &z.slice(px);
+    neigh_mask = neigh_mask | (PX * slice);
+
+    // (1,-1)
+    let slice = &z.slice(s![2.., ..-2]);
+    neigh_mask = neigh_mask | (NX * slice);
+    neigh_mask = neigh_mask | (PY * slice);
+
+    // (1,0)
+    let slice = &z.slice(py);
+    neigh_mask = neigh_mask | (PY * slice);
+
+    // (1,1)
+    let slice = &z.slice(s![2.., 2..]);
+    neigh_mask = neigh_mask | (PX * slice);
+    neigh_mask = neigh_mask | (PY * slice);
+
+    println!("neigh mask");
+    render_hex(&neigh_mask);
+
+    // NOTE(lubo): "First half" - pick the first available direction according to the problem definition
+    let mut pdv = proposition_dirs.slice_mut(s![1..-1, 1..-1]);
+    pdv.zip_mut_with(&neigh_mask, |x, &n| {
+        if (n & OCC) > 0 {
+            *x = OCC;
+        }
+
+        for dir in dir_order.iter() {
+            if (n & OCC) > 0 && (n & dir) == 0 {
+                *x |= *dir;
+                break;
+            }
+        }
+    });
+
+    println!("prop dirs");
+    render_hex(&proposition_dirs);
+
+    // NOTE(lubo): "Second half"
+    // let mut ppv = z.slice_mut(s![1..-1, 1..-1]);
+    let mut ppv = proposition_ps.slice_mut(center);
+
+    let pdv_px = proposition_dirs.slice(px);
+    let pdv_py = proposition_dirs.slice(py);
+    let pdv_nx = proposition_dirs.slice(nx);
+    let pdv_ny = proposition_dirs.slice(ny);
+
+    // NOTE(lubo): Tally guys who want to move...
+    // ... west
+    ppv.zip_mut_with(&pdv_px, |y, &x| {
+        if (x & OCC) > 0 && (x & NX) > 0 {
+            *y += 1;
+        }
+    });
+
+    // ... north
+    ppv.zip_mut_with(&pdv_py, |y, &x| {
+        if (x & OCC) > 0 && (x & NY) > 0 {
+            *y += 1;
+        }
+    });
+
+    // ... east
+    ppv.zip_mut_with(&pdv_nx, |y, &x| {
+        if (x & OCC) > 0 && (x & PX) > 0 {
+            *y += 1;
+        }
+    });
+
+    // ... south
+    ppv.zip_mut_with(&pdv_ny, |y, &x| {
+        if (x & OCC) > 0 && (x & PY) > 0 {
+            *y += 1;
+        }
+    });
+
+    // // ... and those who do not want to move
+    // let pdv_none = proposition_dirs.slice_mut(s![1..-1, 1..-1]);
+    // ppv.zip_mut_with(&pdv_none, |y, &x| {
+    //     if (x & OCC) > 0 && (x & (PX | PY | NX | NY)) > 0 {
+    //         *y -= 100;
+    //     }
+    // });
+
+    Zip::from(&mut move_tos.slice_mut(center))
+        .and(&mut move_froms.slice_mut(px))
+        .and(&ppv)
+        .and(&pdv_px)
+        .for_each(|to, from, &y, &x| {
+            if (x & OCC) > 0 && (x & NX) > 0 && y == 1 {
+                *to += 1;
+                *from += 1;
+            }
+        });
+
+    Zip::from(&mut move_tos.slice_mut(center))
+        .and(&mut move_froms.slice_mut(py))
+        .and(&ppv)
+        .and(&pdv_py)
+        .for_each(|to, from, &y, &x| {
+            if (x & OCC) > 0 && (x & NY) > 0 && y == 1 {
+                *to += 1;
+                *from += 1;
+            }
+        });
+
+    Zip::from(&mut move_tos.slice_mut(center))
+        .and(&mut move_froms.slice_mut(nx))
+        .and(&ppv)
+        .and(&pdv_nx)
+        .for_each(|to, from, &y, &x| {
+            if (x & OCC) > 0 && (x & PX) > 0 && y == 1 {
+                *to += 1;
+                *from += 1;
+            }
+        });
+
+    Zip::from(&mut move_tos.slice_mut(center))
+        .and(&mut move_froms.slice_mut(ny))
+        .and(&ppv)
+        .and(&pdv_ny)
+        .for_each(|to, from, &y, &x| {
+            if (x & OCC) > 0 && (x & PY) > 0 && y == 1 {
+                *to += 1;
+                *from += 1;
+            }
+        });
+
+    println!("move tos");
+    render_hex(&move_tos);
+
+    println!("move from");
+    render_hex(&move_froms);
+
+    println!("do it");
+
+    azip!((z in z, &to in &move_tos, &from in &move_froms) *z += to - from);
+
+    // move_tos.zip_mut_with(&move_froms, |a, b| *a -= b);
+    // render_hex(z);
 }
 
-fn turn_on_corners(z: &mut Board) {
-    let n = z.nrows();
-    let m = z.ncols();
-    z[[1, 1]] = 1;
-    z[[1, m - 2]] = 1;
-    z[[n - 2, 1]] = 1;
-    z[[n - 2, m - 2]] = 1;
+fn render_hex(a: &Board) {
+    for row in a.rows() {
+        for &x in row {
+            if x != 0 {
+                if x & OCC > 0 {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+
+                print!("{:x}", x & !OCC);
+            } else {
+                print!(". ");
+            }
+        }
+        println!();
+    }
 }
 
 fn render(a: &Board) {
     for row in a.rows() {
         for &x in row {
-            if x > 0 {
-                print!("#");
+            if x != 0 {
+                print!("[]");
+                // print!("#");
             } else {
-                print!(".");
+                // print!(".");
+                print!(". ");
             }
         }
         println!();
@@ -83,14 +270,19 @@ impl Problem for Day<23> {
     {
         let mut result = 0;
 
+        println!("North: {NY} (NY)  South: {PY} (PY)  West: {NX} (NX)  East: {PX} (PX)");
+        let mut dir_order = vec![NY, PY, NX, PX];
+
+        // NOTE(lubo): looks good but need to not do anything when no neighbours
         let mut a = parse(INPUT);
-        let mut scratch = Board::zeros((N, N));
+        println!("Initial state");
+        render(&a);
+        println!("--");
         let steps = 100;
-        turn_on_corners(&mut a);
         for _ in 0..steps {
-            iterate(&mut a, &mut scratch);
-            render(&a);
-            turn_on_corners(&mut a);
+            iterate(&mut a, &dir_order);
+            dir_order.rotate_left(1);
+            println!("--");
             render(&a);
         }
         render(&a);
